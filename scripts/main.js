6f066b96f09033b028de04016013e65033f76196
Ilnura Fattakhova <paramore2101@yandex.com>
2021-01-30 03:22:52 +0500

    edit with review

--- scripts/main.js
+++ scripts/main.js
@@ -6,32 +6,25 @@ let container = document.querySelector(".profile__info");
 let previousName = document.querySelector(".profile__name");
 let previousDescription = document.querySelector(".profile__description");
 let like = document.querySelector(".element__like-button");
+let name = form.querySelector(".popup__edit-name");
+let description = form.querySelector(".popup__edit-info");
 
-function displayPopup() {
-  if (
-    edit.addEventListener("click", function () {
-      form.classList.add("popup_opened");
-    })
-  );
-  else if (
-    closeForm.addEventListener("click", function () {
-      form.classList.remove("popup_opened");
-    })
-  );
+function openPopup() {
+  form.classList.add("popup_opened");
+}   
+
+function closePopup() {
+  form.classList.remove("popup_opened");
 }
 
 function editInformation(evt) {
   evt.preventDefault();
-  let name = form.querySelector(".popup__edit-name").value;
-  let description = form.querySelector(".popup__edit-info").value;
-  previousName.textContent = name;
-  previousDescription.textContent = description;
+  previousName.textContent = name.value;
+  previousDescription.textContent = description.value;
+  closePopup();
 }
 
 form.addEventListener("submit", editInformation);
 
-displayPopup();
-
-like.addEventListener("click", function() {
-  like.classList.toggle("element__like-button_active");
-});
+edit.addEventListener("click", openPopup);
+closeForm.addEventListener("click", closePopup);

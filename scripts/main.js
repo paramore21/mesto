7c1b1de8ef0afb1307f98e16ab31b44ca57d0073
Ilnura Fattakhova <paramore2101@yandex.com>
2021-02-10 00:35:51 +0500

    add new popup card

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,52 @@
-let form = document.querySelector(".popup");
-let submit = form.querySelector(".popup__submit");
-let closeForm = form.querySelector(".popup__close-form");
-let edit = document.querySelector(".profile__edit-button");
-let container = document.querySelector(".profile__info");
-let previousName = document.querySelector(".profile__name");
-let previousDescription = document.querySelector(".profile__description");
-let like = document.querySelector(".element__like-button");
-let inputName = document.querySelector(".popup__edit_type_name");
-let inputDescription = document.querySelector(".popup__edit_type_description");
+const form = document.querySelector(".popup");
+const submit = form.querySelector(".popup__submit");
+const closeForm = form.querySelector(".popup__close-form");
+const edit = document.querySelector(".profile__edit-button");
+const container = document.querySelector(".profile__info");
+const previousName = document.querySelector(".profile__name");
+const previousDescription = document.querySelector(".profile__description");
+const like = document.querySelector(".element__like-button");
+const inputName = form.querySelector(".popup__edit_type_name");
+const inputDescription = form.querySelector(".popup__edit_type_description");
+const place = document.querySelector(".place");
+const addPlace = document.querySelector(".profile__add-card");
 
+const initialCards = [
+  {
+    name: 'Река',
+    link: 'https://unsplash.com/photos/9H6PHAs-cAc'
+  },
+  {
+    name: 'Заснеженный берег',
+    link: 'https://unsplash.com/photos/Lap9y7XQcLo'
+  },
+  {
+    name: 'Костер',
+    link: 'https://unsplash.com/photos/X8DALn4RJaA'
+  },
+  {
+    name: 'Водопад',
+    link: 'https://unsplash.com/photos/5-jtsfuaLBw'
+  },
+  {
+    name: 'Млечный путь',
+    link: 'https://unsplash.com/photos/voQDMI8XIJI'
+  },
+  {
+    name: 'Горный хребет вулкана',
+    link: 'https://unsplash.com/photos/KZC7BJo0Cl0'
+  }
+];
 
-function openPopup() {
+function openEdit() {
   form.classList.add("popup_opened");
   inputName.value = previousName.textContent;
   inputDescription.value = previousDescription.textContent;
 }   
+
+function openPlace() {
+  place.classList.add("place_opened");
+}
 
 function closePopup() {
   form.classList.remove("popup_opened");
@@ -28,6 +60,6 @@ function editInformation(evt) {
 }
 
 form.addEventListener("submit", editInformation);
-
-edit.addEventListener("click", openPopup);
+edit.addEventListener("click", openEdit);
 closeForm.addEventListener("click", closePopup);
+addPlace.addEventListener("click", openPlace);

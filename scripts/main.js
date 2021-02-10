263bcc98093a7d7445ad82ce033765cf1b1efaa9
Ilnura Fattakhova <paramore2101@yandex.com>
2021-02-10 23:01:46 +0500

    card render added

--- scripts/main.js
+++ scripts/main.js
@@ -5,47 +5,61 @@ const edit = document.querySelector(".profile__edit-button");
 const container = document.querySelector(".profile__info");
 const previousName = document.querySelector(".profile__name");
 const previousDescription = document.querySelector(".profile__description");
-const like = document.querySelector(".element__like-button");
 const inputName = form.querySelector(".popup__edit_type_name");
 const inputDescription = form.querySelector(".popup__edit_type_description");
 const place = document.querySelector(".place");
+const newPlaceName = place.querySelector(".place__edit_type_place");
+const newPlaceUrl = place.querySelector(".place__efit_type_url");
+const placeSubmit = place.querySelector(".place__submit");
 const addPlace = document.querySelector(".profile__add-card");
+const closePlace = document.querySelector(".place__close-form");
+const elementContainer = document.querySelector(".elements");
 
 const initialCards = [
   {
-    name: 'Река',
-    link: 'https://unsplash.com/photos/9H6PHAs-cAc'
+    name: "Река",
+    link:
+      "https://images.unsplash.com/photo-1611040549039-adc39e805408?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=968&q=80",
   },
   {
-    name: 'Заснеженный берег',
-    link: 'https://unsplash.com/photos/Lap9y7XQcLo'
+    name: "Заснеженный берег",
+    link:
+      "https://images.unsplash.com/photo-1610564823068-b7cb193950b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=966&q=80",
   },
   {
-    name: 'Костер',
-    link: 'https://unsplash.com/photos/X8DALn4RJaA'
+    name: "Костер",
+    link:
+      "https://images.unsplash.com/photo-1612676756023-0219168b193f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=346&q=80Ln4RJaA",
   },
   {
-    name: 'Водопад',
-    link: 'https://unsplash.com/photos/5-jtsfuaLBw'
+    name: "Водопад",
+    link:
+      "https://images.unsplash.com/photo-1508459855340-fb63ac591728?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80s://unsplash.com/photos/5-jtsfuaLBw",
   },
   {
-    name: 'Млечный путь',
-    link: 'https://unsplash.com/photos/voQDMI8XIJI'
+    name: "Млечный путь",
+    link:
+      "https://images.unsplash.com/photo-1596348158371-d3a25ec4dcf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=337&q=80splash.com/photos/voQDMI8XIJI",
   },
   {
-    name: 'Горный хребет вулкана',
-    link: 'https://unsplash.com/photos/KZC7BJo0Cl0'
-  }
+    name: "Горный хребет вулкана",
+    link:
+      "https://images.unsplash.com/photo-1541845157-a6d2d100c931?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80Jo0Cl0",
+  },
 ];
 
 function openEdit() {
   form.classList.add("popup_opened");
   inputName.value = previousName.textContent;
   inputDescription.value = previousDescription.textContent;
-}   
+}
 
 function openPlace() {
   place.classList.add("place_opened");
+}
+
+function closeAddPlace() {
+  place.classList.remove("place_opened");
 }
 
 function closePopup() {
@@ -59,7 +73,95 @@ function editInformation(evt) {
   closePopup();
 }
 
+function likeToggle(ev) {
+  const elem = ev.target;
+  if (elem) {
+    elem.classList.toggle("element__like-button_active");
+  }
+}
+
+function likeEventRegister() {
+  const likeButton = document.querySelectorAll(".element__like-button");
+  likeButton.forEach((elem) => {
+    elem.removeEventListener("click", likeToggle);
+    elem.addEventListener("click", likeToggle);
+  });
+}
+
+function removeEvent(ev) {
+  const button = ev.target;
+  console.log(`hehehehe`);
+  if (button) {
+    const block = button.parentNode;
+    block.parentNode.removeChild(block);
+  }
+}
+
+function deleteImageEvent() {
+  const elementDelete = document.querySelectorAll(".element__delete");
+  elementDelete.forEach((node) => {
+    node.removeEventListener("click", removeEvent);
+    node.addEventListener("click", removeEvent);
+  });
+}
+
+function addCardToContailer(name, link) {
+  const div = document.createElement("div");
+  div.classList.add("element");
+
+  const img = document.createElement("img");
+  img.classList.add("element__image");
+  img.setAttribute("src", link);
+  img.setAttribute("alt", name);
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("element__delete");
+
+  const title = document.createElement("h2");
+  title.classList.add("element__title");
+  title.textContent = name;
+
+  const likeBtn = document.createElement("button");
+  likeBtn.classList.add("element__like-button");
+
+  div.appendChild(img);
+  div.appendChild(buttonDelete);
+  div.appendChild(title);
+  div.appendChild(likeBtn);
+
+  elementContainer.prepend(div);
+}
+
+function render() {
+  initialCards.forEach((el) => {
+    addCardToContailer(el.name, el.link);
+  });
+}
+
+function renderAdded(index) {
+  addCardToContailer(initialCards[index].name, initialCards[index].link);
+}
+
+function addItem() {
+  initialCards.push({ name: newPlaceName.value, link: newPlaceUrl.value });
+  renderAdded(initialCards.length - 1);
+  closeAddPlace();
+  deleteImageEvent();
+  likeEventRegister();
+}
+
+render();
+deleteImageEvent();
+likeEventRegister();
 form.addEventListener("submit", editInformation);
 edit.addEventListener("click", openEdit);
 closeForm.addEventListener("click", closePopup);
 addPlace.addEventListener("click", openPlace);
+placeSubmit.addEventListener("click", addItem);
+closePlace.addEventListener("click", closeAddPlace);
+
+// const elementTemplate = document.querySelector("#element").content;
+// const image = document.querySelector(".element__image").cloneNode(true);
+// const deleteItem = document.querySelector(".element__delete").cloneNode(true);
+// const header = document.querySelector(".element__title").cloneNode(true);
+// const likeButton = document.querySelector(".element__like-button").cloneNode(true);
